a5e6def5b396b550b07ca4a4ddd4c80e27b28a78
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-21 05:20:33 +0530

    updating seed-jobs  repo

--- directory.groovy
+++ directory.groovy
@@ -2,3 +2,8 @@ folder('terraform') {
     displayName('terraform')
     description('terraform jobs')
 }
+
+folder('terraform1') {
+    displayName('terraform')
+    description('terraform jobs')
+}
