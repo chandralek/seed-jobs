2c374d2c9f8ca914a1a89bcc15bcaaee3a32ed12
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-28 22:11:12 +0530

    updating seed-jobs  repo

--- terraform/nonprod/rdsmysql.groovy
+++ terraform/nonprod/rdsmysql.groovy
@@ -1,10 +1,10 @@
-pipelineJob('terraform/nonprod/rds_mysql') {
+pipelineJob('terraform/nonprod/rdsmysql') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/chandralek/rds-mysql.git')
+            'url'('https://github.com/chandralek/rdsmysql.git')
             'credentialsId'('GitUserPass')
           }
         }
